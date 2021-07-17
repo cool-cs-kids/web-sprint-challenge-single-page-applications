dc57f149fb8cc465e4ed97c5a0ae7f65cb471edd
tryingtokeepup <kl.hawaii@gmail.com>
2021-07-16 18:20:16 -0700

    made nav bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+
+// 👉 STEP 2 - React Router imports (Route, Link and Switch)
+import { Route, Link, Switch } from 'react-router-dom';
+// Components used for the different routes
+
+// import App.css
+import './App.css';
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div className="App">
+      <nav>
+        <h1 className="store-header">Lambda Eats</h1>
+        <div className="nav-links">
+          {/* 👉 STEP 3 - Make Links to navigate us Home (`/`) and Shop (`/items-list`) */}
+
+          <Link to="/">Home</Link>
+          <Link to="/help">Help</Link>
+        </div>
+      </nav>
+    </div>
   );
 };
 export default App;
